e9ff31367721dcfbaf028a9ee2d859a0af59f738
„udqoi“ <udqoi@student.kit.edu>
2023-01-17 12:39:57 +0100

    Added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <v-app>
+    <v-app-bar :elevation="2">
+      
+      
+      <v-app-bar-title>lara.</v-app-bar-title>
+    </v-app-bar>
+    <v-btn>Wunderschön</v-btn>
+  </v-app>
 </template>
 
 <style scoped>
