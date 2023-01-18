f4cf289d25842091236640922a9f31498639de4f
„udqoi“ <udqoi@student.kit.edu>
2023-01-18 13:05:12 +0100

    Worked on the Navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,28 +1,71 @@
 <script setup lang="ts">
 
-function logout() {
-    // TODO Log out the user
-    console.log("logout")
+import router from '../router';
+
+// language options from language service
+// TODO get options from language service
+let languages:string[] = [
+    'german',
+    'english'
+];
+
+let selectedLanguage: string;
+
+// Function to logout the user
+function logout(): void {
+    // TODO Implement: Log out the user
+    console.log("logout");
+
+    // Navigate to the login view
+    router.push({ name: 'login' });
 }
 
-function checkAdmin() {
-    // TODO Check if the user is an admin
-    console.log("check Admin")
+// Method to check, if the user is a admin
+function checkAdmin(): boolean {
+    let isAdmin: boolean = true;
+    
+    console.log("Check admin");
+
+    return isAdmin;
 }
 
 </script>
 
 <template>
-    <div class="lara-navbar">
-        <v-app-bar>
-            <span class="lara-navbar-link" @click="checkAdmin()">Nutzer verwalten</span>
-            <router-link class="lara-navbar-link" :to="{ name: 'home' }">Start</router-link>
-            <span class="lara-navbar-link" @click="logout()">Abmelden</span>
+    <div class="lara-navbar-container">
+        <v-app-bar :elevation="5" class="lara-navbar">
+            <div class="ml-6">
+                <v-app-bar-title>
+                    <span class="font-weight-bold">lara.</span>
+                </v-app-bar-title>
+            </div>
+            
+            <v-spacer></v-spacer>
+            <div class="mr-6 lara-navbar-link-collection">
+                <v-menu>
+                    moins
+                </v-menu>
+                <router-link class="lara-navbar-link" v-if="checkAdmin()" :to="{ name: 'admin' }">Nutzer verwalten</router-link>
+                <router-link class="pl-6 lara-navbar-link" :to="{ name: 'home' }">Start</router-link>
+                <span class="pl-6 lara-navbar-link" @click="logout()">Abmelden</span>
+            </div>
         </v-app-bar>
+        
     </div>
 </template>
 
 <style scoped>
+
+.lara-navbar {
+    /*
+    display: flex;
+    justify-content: center;
+    align-items: center;
+*/
+    width: 100vw;
+}
+
+
 
 .lara-navbar-link {
     color: #000;
