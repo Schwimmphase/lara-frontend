8a4e30e76be37d870cf2ca06e1937a02a39f8865
„udqoi“ <udqoi@student.kit.edu>
2023-02-10 15:42:50 +0100

    refactored HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <template>
-
     <v-container class="w-75 pt-8">
         <h1 class="text-h3 font-weight-bold">{{ $t('home.greetings', {username: currentUser!.username}) }}</h1>
 
         <div style="width: 300px">
             <NewResearchDialog :button-text="$t('home.startNewResearch')" @save="(title, description) => onCreateResearch(title, description)">
-                <lara-button class="mt-8 mb-8" type="primary" @click="">{{ $t('home.startNewResearch') }}</lara-button>
+                <lara-button class="mt-8 mb-8" type="primary">{{ $t('home.startNewResearch') }}</lara-button>
             </NewResearchDialog>
         </div>
 
@@ -21,33 +20,35 @@
                                :enqueued="420"
                                :started-at="research.startDate.toLocaleDateString()"
                                :research="research"
-                               @data-change="(newTitle: string, newDescription: string) => onEditResearch(research, newTitle, newDescription)"
+                               @data-change="(newTitle: string, newDescription: string) => onUpdateResearch(research, newTitle, newDescription)"
                                @delete="onDeleteResearch(research)"
                 />
             </div>
         </div>
     </v-container>
-
 </template>
 
 
 <script setup lang="ts">
 
+import type {Research} from "@/model/Research";
+import { Comment } from "@/model/Comment";
+
 import ResearchCard from "@/components/cards/ResearchCard.vue";
 import LaraButton from "@/components/basic/LaraButton.vue";
-import type {Research} from "@/model/Research";
+import NewResearchDialog from "@/components/dialogs/NewResearchDialog.vue";
+
 import { useCurrentUserStore } from "@/stores/currentUser";
 import { ResearchApiHandler } from "@/api/Research/ResearchApiHandler";
 import { useOpenResearchStore } from "@/stores/openResearch";
 import { useOpenPaperStore } from "@/stores/openPaper";
-import NewResearchDialog from "@/components/dialogs/NewResearchDialog.vue";
-import { Comment } from "@/model/Comment";
+
 import { reactive } from "vue";
 import {i18n} from "@/internationalization/i18n";
 
 document.title = i18n.global.t("pageTitles.home") + " - lara";
 
-// reset open research/paper
+// Reset open research/paper, in Home view there is no open Paper / open Research
 useOpenResearchStore().resetStore();
 useOpenPaperStore().resetStore();
 
@@ -63,14 +64,13 @@ async function getResearches() {
         state.researches.push(research);
     });
 }
-getResearches();
 
 async function onCreateResearch(title: string, description: string) {
     const research = await ResearchApiHandler.createResearch(title, new Comment(description));
     state.researches.push(research);
 }
 
-async function onEditResearch(research: Research, title: string, description: string) {
+async function onUpdateResearch(research: Research, title: string, description: string) {
     const newResearch = await ResearchApiHandler.updateResearch(research, title, new Comment(description));
     state.researches[state.researches.indexOf(research)] = newResearch;
 }
@@ -79,6 +79,9 @@ async function onDeleteResearch(research: Research) {
     await ResearchApiHandler.deleteResearch(research);
     state.researches.splice(state.researches.indexOf(research), 1);
 }
+
+getResearches();
+
 </script>
 
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -21,6 +21,7 @@
 
 
 <script setup lang="ts">
+
 import { reactive } from 'vue';
 import LaraButton from '@/components/basic/LaraButton.vue';
 import { AuthApiHandler } from '@/api/Auth/AuthApiHandler';
